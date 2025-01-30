899d824b61f18cd4668297493b30b18a21de1d16
Meiyo01 <khanyilesiyanda01@gmail.com>
2025-01-31 00:56:59 +0200

    - new error : get absolute url

--- Factions/urls.py
+++ Factions/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'Factions'
 
 urlpatterns = [
-    path('', views.GovernmentListView.as_view(), name='government.list'),
-    path('<int:year>/<int:month>/<int:day>/<str:name>', views.Government_detail, name='Government_detail'),
+    path('', views.Home.as_view(), name='home'),
+    path('Government/', views.GovernmentList.as_view(), name='government_list'),
+    path('Government/List/', views.Government_detail, name='Government_detail'),
 ]
--- Factions/views.py
+++ Factions/views.py
@@ -2,17 +2,19 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from .models import Government
 
-class GovernmentListView(ListView):
-    queryset = Government.objects.all()
+class Home(ListView):
+    queryset = Government.objects.all
+    template_name = 'Home/index.html'
+class GovernmentList(ListView):
+    queryset = Government.objects.all
     context_object_name = 'Government'
-    paginate_by = 3
-    template_name = 'Factions/Governments/list.html'
+    template_name = 'Government/list.html'
     
 def Government_detail(request, year, month, day, name):
-    gov = get_object_or_404(Government,  name = Government.name,
+    gov = get_object_or_404(Government, slug=gov,
                             added__year= year,
                             added__month= month,
                             added__day = day,
                             )
-    return render(request, 'Factions/Governments/detail.html',
+    return render(request, 'Governments/detail.html',
                   {'Government': gov})
